bc8b3c1c11d6f169abff2837ce60dd120fa860f3
Md Alhaz Mondal Hredhay <60058949+hredhayxz@users.noreply.github.com>
2023-08-14 20:56:21 +0600

    GetX implement on completed task has been done

--- task_manager/lib/ui/screens/completed_task_screen.dart
+++ task_manager/lib/ui/screens/completed_task_screen.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:task_manager/data/models/network_response.dart';
 import 'package:task_manager/data/models/task_list_model.dart';
 import 'package:task_manager/data/services/network_caller.dart';
 import 'package:task_manager/data/utility/urls.dart';
 import 'package:task_manager/ui/screens/update_task_status_sheet.dart';
+import 'package:task_manager/ui/state_managers/get_task_controller.dart';
 import 'package:task_manager/ui/widgets/screen_background.dart';
 import 'package:task_manager/ui/widgets/task_list_tile.dart';
 import 'package:task_manager/ui/widgets/user_profile_appbar.dart';
@@ -15,185 +17,178 @@ class CompletedTaskScreen extends StatefulWidget {
   State<CompletedTaskScreen> createState() => _CompletedTaskScreenState();
 }
 
-class _CompletedTaskScreenState extends State<CompletedTaskScreen> {
-  bool _completedTasksInProgress = false;
-  TaskListModel _taskListModel = TaskListModel();
-
-  Future<void> completedTasks() async {
-    _completedTasksInProgress = true;
-    if (mounted) {
-      setState(() {});
-    }
-    final NetworkResponse response =
-        await NetworkCaller().getRequest(Urls.completedTasks);
-    if (response.isSuccess) {
-      _taskListModel = TaskListModel.fromJson(response.body!);
-    } else {
-      if (mounted) {
-        ScaffoldMessenger.of(context).showSnackBar(
-            const SnackBar(content: Text('Completed tasks get failed')));
+class _CompletedTaskScreenState extends State<CompletedTaskScreen> {final GetTasksController _getTasksController = Get.find<GetTasksController>();
+@override
+void initState() {
+  super.initState();
+  WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+    _getTasksController.getTasks(Urls.completedTasks).then((value) {
+      if (value == false) {
+        Get.snackbar(
+          'Failed',
+          'Completed task data get failed!',
+          backgroundColor: Colors.red,
+          colorText: Colors.white,
+          borderRadius: 10,
+        );
       }
-    }
-    _completedTasksInProgress = false;
-    if (mounted) {
-      setState(() {});
-    }
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
-      completedTasks();
     });
-  }
+  });
+}
 
-  Future<void> deleteTask(String taskId) async {
-    final NetworkResponse response =
-        await NetworkCaller().getRequest(Urls.deleteTask(taskId));
-    if (response.isSuccess) {
-      _taskListModel.data!.removeWhere((element) => element.sId == taskId);
-      if (mounted) {
-        setState(() {
-          ScaffoldMessenger.of(context).showSnackBar(
-              const SnackBar(content: Text('Deletion successful!')));
-        });
-      }
-    } else {
-      if (mounted) {
+Future<void> deleteTask(String taskId) async {
+  final NetworkResponse response =
+  await NetworkCaller().getRequest(Urls.deleteTask(taskId));
+  if (response.isSuccess) {
+    _getTasksController.taskListModel.data!.removeWhere((element) => element.sId == taskId);
+    if (mounted) {
+      setState(() {
         ScaffoldMessenger.of(context).showSnackBar(
-            const SnackBar(content: Text('Deletion of task has been failed')));
-      }
+            const SnackBar(content: Text('Deletion successful!')));
+      });
+    }
+  } else {
+    if (mounted) {
+      ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Deletion of task has been failed')));
     }
   }
+}
 
-  void showStatusUpdateBottomSheet(TaskData task) {
-    showModalBottomSheet(
-      isScrollControlled: true,
-      context: context,
-      builder: (context) {
-        return UpdateTaskStatusSheet(
-            task: task,
-            onUpdate: () {
-              completedTasks();
-            });
-      },
-    );
-  }
+void showStatusUpdateBottomSheet(TaskData task) {
+  showModalBottomSheet(
+    isScrollControlled: true,
+    context: context,
+    builder: (context) {
+      return UpdateTaskStatusSheet(
+          task: task,
+          onUpdate: () {
+            _getTasksController.getTasks(Urls.completedTasks);
+          });
+    },
+  );
+}
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: ScreenBackground(
-          child: Column(
-            children: [
-              const UserProfileAppBar(),
-              Expanded(
-                child: _completedTasksInProgress
-                    ? const Center(
-                        child: CircularProgressIndicator(),
-                      )
-                    : ListView.separated(
-                        itemCount: _taskListModel.data?.length ?? 0,
-                        itemBuilder: (context, index) {
-                          return TaskListTile(
-                            data: _taskListModel.data![index],
-                            onDeleteTap: () {
-                              deleteAlertDialogue(index);
-                            },
-                            onEditTap: () {
-                              editAlertDialogue(index);
-                            },
-                          );
-                        },
-                        separatorBuilder: (BuildContext context, int index) {
-                          return const Divider(
-                            height: 4,
-                          );
-                        },
-                      ),
-              ),
-            ],
-          ),
+@override
+Widget build(BuildContext context) {
+  return Scaffold(
+    body: SafeArea(
+      child: ScreenBackground(
+        child: Column(
+          children: [
+            const UserProfileAppBar(),
+            Expanded(
+              child: GetBuilder<GetTasksController>(
+                  builder: (getTasksController) {
+                    return getTasksController.getTaskInProgress
+                        ? const Center(
+                      child: CircularProgressIndicator(),
+                    )
+                        : ListView.separated(
+                      itemCount:
+                      getTasksController.taskListModel.data?.length ??
+                          0,
+                      itemBuilder: (context, index) {
+                        return TaskListTile(
+                          data:
+                          getTasksController.taskListModel.data![index],
+                          onDeleteTap: () {
+                            deleteAlertDialogue(index);
+                            //deleteTask(_taskListModel.data![index].sId!);
+                          },
+                          onEditTap: () {
+                            editAlertDialogue(index);
+                          },
+                        );
+                      },
+                      separatorBuilder: (BuildContext context, int index) {
+                        return const Divider(
+                          height: 4,
+                        );
+                      },
+                    );
+                  }),
+            ),
+          ],
         ),
       ),
-    );
-  }
+    ),
+  );
+}
 
-  void deleteAlertDialogue(int index) {
-    showDialog(
-      context: context,
-      builder: (ctx) => AlertDialog(
-        title: const Text(
-          "Alert",
-          style: TextStyle(
-            color: Colors.red,
-            fontWeight: FontWeight.bold,
-            // Set the title text color here
-          ),
+void deleteAlertDialogue(int index) {
+  showDialog(
+    context: context,
+    builder: (ctx) => AlertDialog(
+      title: const Text(
+        "Alert",
+        style: TextStyle(
+          color: Colors.red,
+          fontWeight: FontWeight.bold,
+          // Set the title text color here
         ),
-        content: const Text(
-          "Are you want to delete this item?",
-          style: TextStyle(
-            color: Colors.black, // Set the content text color here
-          ),
-        ),
-        backgroundColor: Colors.white,
-        actions: [
-          TextButton(
-            onPressed: () {
-              Navigator.of(ctx).pop();
-            },
-            child: const Text('No'),
-          ),
-          TextButton(
-            onPressed: () {
-              Navigator.of(ctx).pop();
-              deleteTask(_taskListModel.data![index].sId!);
-            },
-            child: const Text('Yes'),
-          ),
-        ],
       ),
-    );
-  }
+      content: const Text(
+        "Are you want to delete this item?",
+        style: TextStyle(
+          color: Colors.black, // Set the content text color here
+        ),
+      ),
+      backgroundColor: Colors.white,
+      actions: [
+        TextButton(
+          onPressed: () {
+            Navigator.of(ctx).pop();
+          },
+          child: const Text('No'),
+        ),
+        TextButton(
+          onPressed: () {
+            Navigator.of(ctx).pop();
+            deleteTask(_getTasksController.taskListModel.data![index].sId!);
+          },
+          child: const Text('Yes'),
+        ),
+      ],
+    ),
+  );
+}
 
-  void editAlertDialogue(int index) {
-    showDialog(
-      context: context,
-      builder: (ctx) => AlertDialog(
-        title: const Text(
-          "Alert",
-          style: TextStyle(
-            color: Colors.red,
-            fontWeight: FontWeight.bold,
-            // Set the title text color here
-          ),
+void editAlertDialogue(int index) {
+  showDialog(
+    context: context,
+    builder: (ctx) => AlertDialog(
+      title: const Text(
+        "Alert",
+        style: TextStyle(
+          color: Colors.red,
+          fontWeight: FontWeight.bold,
+          // Set the title text color here
         ),
-        content: const Text(
-          "Are you want to edit status of this item?",
-          style: TextStyle(
-            color: Colors.black, // Set the content text color here
-          ),
-        ),
-        backgroundColor: Colors.white,
-        actions: [
-          TextButton(
-            onPressed: () {
-              Navigator.of(ctx).pop();
-            },
-            child: const Text('No'),
-          ),
-          TextButton(
-            onPressed: () {
-              Navigator.of(ctx).pop();
-              showStatusUpdateBottomSheet(_taskListModel.data![index]);
-            },
-            child: const Text('Yes'),
-          ),
-        ],
       ),
-    );
-  }
+      content: const Text(
+        "Are you want to edit status of this item?",
+        style: TextStyle(
+          color: Colors.black, // Set the content text color here
+        ),
+      ),
+      backgroundColor: Colors.white,
+      actions: [
+        TextButton(
+          onPressed: () {
+            Navigator.of(ctx).pop();
+          },
+          child: const Text('No'),
+        ),
+        TextButton(
+          onPressed: () {
+            Navigator.of(ctx).pop();
+            showStatusUpdateBottomSheet(_getTasksController.taskListModel.data![index]);
+          },
+          child: const Text('Yes'),
+        ),
+      ],
+    ),
+  );
+}
 }
